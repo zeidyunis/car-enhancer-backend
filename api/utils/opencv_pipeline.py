d566fcf8b87516aea8840af22999a678515f9d1b
zeidyunis <yunis.zeid@gmail.com>
2026-01-29 00:27:22 +0400

    Update opencv_pipeline.py

--- api/utils/opencv_pipeline.py
+++ api/utils/opencv_pipeline.py
@@ -6,9 +6,9 @@ def enhance_image(img: Image.Image) -> np.ndarray:
         img = img.convert("RGB")
 
     img = ImageOps.autocontrast(img, cutoff=1)
-    img = ImageEnhance.Brightness(img).enhance(1.02)
-    img = ImageEnhance.Contrast(img).enhance(1.05)
-    img = ImageEnhance.Color(img).enhance(1.03)
-    img = ImageEnhance.Sharpness(img).enhance(1.02)
+    img = ImageEnhance.Brightness(img).enhance(1.01)
+    img = ImageEnhance.Contrast(img).enhance(1.04)
+    img = ImageEnhance.Color(img).enhance(1.02)
+    img = ImageEnhance.Sharpness(img).enhance(1.01)
 
     return np.array(img)
