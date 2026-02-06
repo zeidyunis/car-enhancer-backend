bbe0d1408c45c5d7982cb0c178ab2e9ee436e646
zeidyunis <yunis.zeid@gmail.com>
2026-02-06 13:17:34 +0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -20,6 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://be57ce02-6783-4807-a967-7ede7043ec97.lovableproject.com",
+        "https://id-preview--be57ce02-6783-4807-a967-7ede7043ec97.lovable.app",
     ],
     allow_credentials=False,
     allow_methods=["*"],
